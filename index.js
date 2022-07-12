2b9f2d12c474d1de177d55206595472c195e9b01
kubstar <52975077+kubstar@users.noreply.github.com>
2022-07-12 19:48:49 +0530

    express npm pack added+index file

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+//init the app
+const app = express()
+
+//connection block
+const PORT = process.env.PORT || 5000
+app.listen(
+    PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+)
